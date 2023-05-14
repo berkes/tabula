77dc6761d711258303090f20c41fc02beeb36f6b
Bèr Kessels <ber@berk.es>
2023-05-14 10:56:51 +0200

    feat: proof of concept to parse, filter and summarize a text ledger

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
 use account::aggregate::Account;
 use async_trait::async_trait;
 use cqrs_es::{EventEnvelope, Query};
+use ledger_parser::{Transaction, LedgerItem};
+use std::fs;
 
 mod account;
+
+fn main() {
+    let ledger_content = fs::read_to_string("/home/ber/tmp/test.ledger").unwrap();
+    let ledger = ledger_parser::parse(&ledger_content).unwrap();
+    let summary: Vec<Transaction> = ledger
+        .items
+        .into_iter()
+        .filter_map(|item| {
+            if let LedgerItem::Transaction(tx) = item {
+                Some(tx)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let vat_summary = filter_transactions(summary, "btw");
+
+    for line in vat_summary  {
+        println!("{}", line);
+    }
+}
+
+fn filter_transactions(transactions: Vec<Transaction>, posting_account_contains: &str) -> Vec<Transaction> {
+    transactions.into_iter().filter_map(|transaction| -> Option<Transaction> {
+        if transaction.postings.iter().any(|posting| posting.account.contains(posting_account_contains)) {
+            Some(transaction)
+        } else {
+            None
+        }
+    }).collect()
+}
 
 pub struct SimpleLoggingQuery {}
 #[async_trait]
@@ -15,9 +49,6 @@ impl Query<Account> for SimpleLoggingQuery {
             )
         }
     }
-}
-fn main() {
-    println!("Hello, world!");
 }
 
 #[cfg(test)]
