f94891bdbbe4c97ddcfcc64e149588b11b497a67
Bèr Kessels <ber@berk.es>
2023-06-01 18:34:40 +0200

    feat: PoC to see how we get the amounts and postings into the ledger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use beancount_core::{directives::Transaction, Date, Flag, Ledger};
+use beancount_core::{directives::Transaction, Date, Flag, Ledger, Posting, Account, AccountType::Assets, IncompleteAmount};
 use beancount_render::render;
-use std::{fs::OpenOptions, io::Write};
+use std::{fs::OpenOptions, io::Write, borrow::Cow};
 
 mod cli;
 
@@ -9,10 +9,36 @@ fn main() {
 
     let today: Date<'static> = chrono::Local::now().date_naive().into();
 
+    let accts_recievable = Account::builder()
+        .ty(Assets)
+        .parts(vec![Cow::Borrowed("AccountsReceivable")])
+        .build();
+
+    let work = Account::builder()
+        .ty(beancount_core::AccountType::Income)
+        .parts(vec![Cow::Borrowed("Work")])
+        .build();
+
+    let amt = IncompleteAmount::builder()
+        .num(Some(1337.into()))
+        .currency(Some(Cow::Borrowed("USD")))
+        .build();
+
+    let postings = vec![
+        Posting::builder()
+            .account(accts_recievable)
+            .units(amt.clone())
+            .build(),
+        Posting::builder()
+            .account(work)
+            .units(amt)
+            .build()
+    ];
     let tx = Transaction::builder()
         .flag(Flag::Warning)
         .date(today)
         .narration("Invoice #1".into())
+        .postings(postings)
         .build();
 
     let ledger = Ledger {
