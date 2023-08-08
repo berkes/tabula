79b9323a1270cb0ea452d7270935566c402ddeb6
Bèr Kessels <ber@berk.es>
2023-08-08 14:57:08 +0200

    chore: Remove the PoC event sourced submodule

--- src/account/command.rs
+++
@@ -1,8 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub enum Command {
-    OpenAccount { account_id: String },
-    DepositMoney { amount: f64, currency: String },
-    WithdrawMoney { amount: f64, currency: String },
-}
